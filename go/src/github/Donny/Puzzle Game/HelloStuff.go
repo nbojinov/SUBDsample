b1f0108d02c84469f02a694dd05c868b0ae44020
Donny Mitsov <donny@linuxmint.linuxmint-domain>
2014-03-27 04:20:18 +0200

    This is my latest commit

--- go/src/github/Donny/Puzzle Game/HelloStuff.go
+++ go/src/github/Donny/Puzzle Game/HelloStuff.go
@@ -1,62 +1,62 @@
 package main
 
 import (
-	
+	"appengine"
 	"html/template"
 	"net/http"
-	"strings"
-	"appengine"
-	"appengine/image"
-	"encoding/json"
 )
 
 type Image struct{
-	urlImage string;
-}
-
-func (Image) Error() (string){
-	return "I am starting to get angry"
+	UrlImage []string  
 }
 
 
 func init(){
 	http.HandleFunc("/uploadForm",uploadForm)
 	http.HandleFunc("/upload",upload)
+	http.HandleFunc("/getPuzzle",handleGetUploadedImages)
+	http.HandleFunc("/login",handleLogin)
+	http.HandleFunc("/signUp",handleSignUp)
+	http.HandleFunc("/updateScore",handleUpdateHighscore)
 }
 
 const uploadTemplate = "<html><body><form action='{{.}}' method='POST' enctype='multipart/form-data'>Upload File: <input type='file' name='file'><br><input type='submit' name='submit' value='Submit'></form></body></html>";
 func uploadForm(rw http.ResponseWriter, r *http.Request){
-	uploadUrl := UploadImageUrl(r)	
+	uploadUrl := UploadImageUrl(rw,	r)	
 
 	template.Must(template.New("upload").Parse(uploadTemplate)).Execute(rw,uploadUrl)
 }
 
 func upload(rw http.ResponseWriter, r *http.Request){
-	UploadImage(r)
+	UploadImage(rw,r)
 	http.Redirect(rw,r,"/uploadForm",http.StatusFound)
 }
 
-func handleUploadedImages(rw http.ResponseWriter,r *http.Request){
+func handleGetUploadedImages(rw http.ResponseWriter,r *http.Request){
+	image := GetAllUploadedImages(rw,r)
+	returnPuzzle(image,rw)
+}
 
+func handleLogin(rw http.ResponseWriter, r *http.Request){
+	
+}
+
+func handleSignUp(rw http.ResponseWriter,r *http.Request){
 	context := appengine.NewContext(r)
-	blobKeys := GetAllUploadedImages(r);
-	blobKsForUrl := strings.Split(blobKeys,", ")
-	imageUrls  := new(string)
-	images := make([]Image,0)
+	username := r.FormValue("username")
+	if r.FormValue("pass") == r.FormValue("repeat_pass"){
+		pass := r.FormValue("pass")
 
-	for _,key := range blobKsForUrl{
-		blob := appengine.BlobKey(key)
-		url,err := image.ServingURL(context,blob,nil)
-		
-		if err != nil{
+		if err := AddUser(context,username,pass); err != nil{
 			http.Error(rw,err.Error(),http.StatusInternalServerError)
 		}
 
-		image := []Image{Image{url.String()}}	
-		res :=append(images,image)
+	} else {
+		http.Error(rw,"Passwords don't match",http.StatusInternalServerError)
 	}
-
-	imageUrls += json.Marshal(images)
-	fmt.Fprintf(rw,imageUrls)
+	http.Redirect(rw,r,"/PuzzleGame.html",http.StatusFound)
 }
 
+func handleUpdateHighscore(rw http.ResponseWriter, r *http.Request){
+	
+}
--- go/src/github/Donny/Puzzle Game/Model.go
+++ go/src/github/Donny/Puzzle Game/Model.go
@@ -1,73 +1,133 @@
 package main;
 
 import(
+	"errors"
 	"appengine/blobstore"
 	"appengine/datastore"
 	"appengine"
+	"appengine/image"
 	"net/http"
 	"net/url"
+//	"fmt"
 )
 
 type Puzzles struct{
 	PuzzleName string
-	imageBlobKeys string
+	ImageBlobKeys string
+}
 
+type User struct{
+	Username string
+	Password string
 }
 
 
-func UploadImageUrl(r *http.Request) (*url.URL){
+func UploadImageUrl(rw http.ResponseWriter,r *http.Request) (*url.URL){
 	context := appengine.NewContext(r)
 	url, err := blobstore.UploadURL(context,"/upload",nil)
 
 	if err != nil{
-
+		http.Error(rw,err.Error(),http.StatusInternalServerError)
 	}
 
 	return url;
 } 
 
 
-func UploadImage(r *http.Request){
+func UploadImage(rw http.ResponseWriter,r *http.Request){
 	context := appengine.NewContext(r)
 	blobs,_, err := blobstore.ParseUpload(r)
 	if  err != nil{
-
+		http.Error(rw,err.Error(),http.StatusInternalServerError)
 	}
 
 	file := blobs["file"]
 	if len(file) == 0{
-
+		http.Error(rw,"File isn't uploaded",http.StatusInternalServerError)
 	}
 
-	puzzles := new(Puzzles)
-	puzzleImages := make([]Puzzles,0);
+	puzzleImages := Puzzles{}
+	puzzleImages.PuzzleName = "First Puzzle"
+	puzzleImages.ImageBlobKeys = string(file[0].BlobKey)
 
-	query := datastore.NewQuery("Puzzles")
-	newKeyQ,errQ := query.GetAll(context,puzzleImages)
-	if errQ != nil{
+	if _,err:=datastore.Put(context,
+		datastore.NewIncompleteKey(context,"Puzzles",ParentDatastoreKey(context,"Puzzles","puzzle_images")),&puzzleImages);
+	err != nil{
+		http.Error(rw,err.Error(),http.StatusInternalServerError)		
+	} 
 
-	}
 
-	key := datastore.NewKey(context,"Puzzles","stringID",0,nil)
-	
-	if len(puzzleImages) == 0 {
-		puzzles.PuzzleName = "First Puzzle"
-		puzzles.imageBlobKeys = string(file[0].BlobKey) +", "
-		if _,err:= datastore.Put(context,key,&puzzles); err != nil{
-
-		}
-	} else {
-		puzzleImages[0].imageBlobKeys += string(file[0].BlobKey) + ", "
-		datastore.Put(context,newKeyQ[0],&puzzleImages[0])
-	}
 }
 
-func GetAllUploadedImages(r *http.Request) (string){
+func GetAllUploadedImages(rw http.ResponseWriter,r *http.Request) (img Image){
 	 context := appengine.NewContext(r)
-	 query := datastore.NewQuery("Puzzles")
-	 puzzles := make([]Puzzles,0)
-	 _,errQ := query.GetAll(context,puzzles)
+	 query := datastore.NewQuery("Puzzles").Ancestor(ParentDatastoreKey(context,"Puzzles","puzzle_images")).Filter("PuzzleName=","First Puzzle").Limit(8)
+	 puzzles := make([]Puzzles,0,8)
+	 images := make([]string,8,8)
+	 _,errq := query.GetAll(context,&puzzles)
 
-	 blobKeys := puzzles[0].imageBlobKeys;
-	 return blobKeys;
+	 if errq != nil{
+	 	http.Error(rw,errq.Error(),http.StatusInternalServerError)
+	 }
+
+	 num := 0
+
+	 for _,puzzleImage:= range puzzles{
+	 	blob:=appengine.BlobKey(puzzleImage.ImageBlobKeys)
+	 	url,err := image.ServingURL(context,blob,nil)
+	 	if err!=nil{
+	 		http.Error(rw,err.Error(),http.StatusInternalServerError)
+	 		return 
+	 	}
+	 	images[num] = url.String();
+	 	num++
+	 }
+
+	 img.UrlImage = images
+	 return 
 }
+
+func ParentDatastoreKey(context appengine.Context, entityName string, databaseName string) *datastore.Key{
+	return datastore.NewKey(context, entityName, databaseName, 0, nil);
+}
+
+//adding a user
+func AddUser(context appengine.Context, username string, pass string) (error) {
+	users := make([]User,0,1)
+	q := datastore.NewQuery("User").Ancestor(ParentDatastoreKey(context,"User","game_users")).
+			Filter("Username=",username).Filter("Password=",pass)
+	if _,err := q.GetAll(context,&users); err != nil {
+		return err
+	} else if len(users) >= 1{
+		return errors.New("There are already other users")		
+	}
+
+	user := User{
+		Username: username,
+		Password: pass,
+	}
+
+	if _,err := datastore.Put(context,
+		datastore.NewIncompleteKey(context,"User",ParentDatastoreKey(context,"User","game_users")),&user);
+	err != nil{
+		return err
+	}
+
+	return nil
+}
+
+
+func checkIfUser(context appengine.Context, username string, pass string) (bool,error){
+
+	q := datastore.NewQuery("User").Ancestor(ParentDatastoreKey(context,"User","game_users")).Filter("Username=",username).
+			Filter("Password=",pass)
+	user := make([]User,0,1)
+
+	if _,err := q.GetAll(context,&user); err != nil || len(user) == 0 {
+		return false,err
+	} else {
+		return true,nil
+	}
+
+}
+
